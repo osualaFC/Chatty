ea6509e1a040b1a44f6df7b93c97266f3367dbb6
osualaFC <fredrick.osuala@decagon.dev>
2020-08-08 22:04:57 +0100

    added chatmessage widget

---
+++ lib/ChatMessage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:chatty/ChatScreen.dart';
+
+class ChatMessage extends StatelessWidget {
+
+  ChatMessage({this.text});
+  final String text;
+  String _name = 'Uzo';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(_name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
