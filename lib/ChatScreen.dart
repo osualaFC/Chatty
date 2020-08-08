a95a1dde6389ccc860da08eb647fb15c87021a58
osualaFC <fredrick.osuala@decagon.dev>
2020-08-08 21:47:36 +0100

    buildTextComposer updated with themes and flexible textfield

--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -7,6 +7,7 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
+
 //clear fields and read its value
   void _handleSubmitted(String text) {
     _textController.clear();
@@ -14,15 +15,28 @@ class _ChatScreenState extends State<ChatScreen> {
 
 //  The TextField with the TextEditingController gives you control over the text field. This controller will clear the field and read its value.
   Widget _buildTextComposer() {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
-      child: Row(
-        children: <Widget>[TextField(
-          controller: _textController,
-          onSubmitted: _handleSubmitted,
-          decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            //Wrapping the TextField in a Flexible widget tells the Row to automatically size the text field to use the remaining space that isn't used by the button.
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            )
+          ],
         ),
-      ],
       ),
     );
   }
