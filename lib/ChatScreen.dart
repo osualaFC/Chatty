d8adef0211716f887535ad065a544bf4a2fe9d32
osualaFC <fredrick.osuala@decagon.dev>
2020-08-08 20:49:41 +0100

    chatscreen widget added

--- lib/ChatScreen.dart
+++ lib/ChatScreen.dart
@@ -7,24 +7,33 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
+//clear fields and read its value
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
 
+//  The TextField with the TextEditingController gives you control over the text field. This controller will clear the field and read its value.
   Widget _buildTextComposer() {
-    return  Container(
+    return Container(
       margin: EdgeInsets.symmetric(horizontal: 8.0),
-      child: TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: InputDecoration.collapsed(
-            hintText: 'Send a message'),
+      child: Row(
+        children: <Widget>[TextField(
+          controller: _textController,
+          onSubmitted: _handleSubmitted,
+          decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+        ),
+      ],
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Friendly chat"),
       ),
+      body: _buildTextComposer(),
     );
   }
 }
